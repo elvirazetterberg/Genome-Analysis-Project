2d9264d073589e76f83a082aca0545d6a470921d
elvirazetterberg <elvira.zetterberg@gmail.com>
2023-04-19 14:51:14 +0200

    cleaning more

--- results/artemis.sh
+++
@@ -1,6 +0,0 @@
-# canu pilon
-# act /home/elze3417/genome_analysis/results/canu_pilon/pilon.fasta results.blastn /home/elze3417/genome_analysis/data/ncbi_dataset/data/GCA_009734005.2/GCA_009734005.2_ASM973400v2_genomic.fna
-
-# spades
-act /home/elze3417/genome_analysis/results/spades/contigs.fasta results_spades.blastn /home/elze3417/genome_analysis/data/ncbi_dataset/data/GCA_009734005.2/GCA_009734005.2_ASM973400v2_genomic.fna
-
